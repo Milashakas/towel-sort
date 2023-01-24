c975a05afd41796dca75e67ed1a999826bfbfb29
Milana Kasumava <milanamamedova52@mail.ru>
2023-01-24 19:45:30 +0300

    feat:make js task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix==undefined){
+    return []
+  }
+let arr=[];
+for (let i=0;i<=matrix.length-1;i++) {
+  if (i==0 || i%2==0) {
+    for (let j=0;j<=matrix[i].length-1;j++){
+      arr.push(matrix[i][j]);
+    }
+   } else {
+      for (let c=matrix[i].length-1;c>=0;c--){
+        arr.push(matrix[i][c]);
+    }
+  }
+}
+return arr
 }
